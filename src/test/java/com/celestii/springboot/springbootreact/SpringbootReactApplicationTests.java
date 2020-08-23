7770672b6d7487d4fa23326fa922c4b0b9739aab
celestii <lshh459@gmail.com>
2020-08-24 00:05:32 +0900

    Initial commit

---
+++ src/test/java/com/celestii/springboot/springbootreact/SpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celestii.springboot.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
