6f9bb3d5b34913b9a243290f1d48f37a2c948a26
celestii <lshh459@gmail.com>
2020-08-25 00:48:30 +0900

    HikariDataSource, MySql 설정

---
+++ src/test/java/com/celestii/springboot/springbootreact/config/DbConfigTest.java
@@ -0,0 +1,32 @@
+package com.celestii.springboot.springbootreact.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbConfigTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() {
+        try (Connection con = dataSource.getConnection()) {
+            log.info("success testConnection");
+            log.info(String.valueOf(con));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
